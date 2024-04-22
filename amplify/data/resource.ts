c365426f0347ca2b76b0f700753d89f7e4949d4a
ykethan <ykethan@amazon.com>
2024-04-22 15:49:46 -0400

    apikey

--- amplify/data/resource.ts
+++ amplify/data/resource.ts
@@ -43,6 +43,9 @@ export const data = defineData({
   schema,
   authorizationModes: {
     defaultAuthorizationMode: "iam",
+    apiKeyAuthorizationMode: {
+      expiresInDays: 17,
+    },
   },
 });
 
