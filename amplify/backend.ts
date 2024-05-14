55a54b487fc452634081ae9e51851a06170ea5ad
ykethan <ykethan@amazon.com>
2024-05-15 00:14:57 +0530

    rm function

--- amplify/backend.ts
+++ amplify/backend.ts
@@ -1,7 +1,6 @@
 import { defineBackend } from "@aws-amplify/backend";
 import { auth } from "./auth/resource";
 import { data } from "./data/resource";
-import { sayHello } from "./function/resource";
 import { storage } from "./storage/resource";
 
 /**
@@ -11,5 +10,5 @@ const backend = defineBackend({
   auth,
   data,
   storage,
-  sayHello,
+  // sayHello,
 });
