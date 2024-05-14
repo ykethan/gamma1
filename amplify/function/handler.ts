57500521b37a2cad101b8426d1c6885f1f3e85c6
ykethan <ykethan@amazon.com>
2024-05-15 00:21:25 +0530

    rm function

--- amplify/function/handler.ts
+++ amplify/function/handler.ts
@@ -1,7 +1,7 @@
-import { env } from "$amplify/env/say-hello";
+// import { env } from "$amplify/env/say-hello";
 
 export const handler = async (event: any) => {
-  console.log(env.NAME);
+  //   console.log(env.NAME);
   // your function code goes here
   return "Hello, World!";
 };
